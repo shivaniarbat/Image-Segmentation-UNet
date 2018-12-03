5c57aa2d33a55d330ce83013c21d04e41112d902
Shivani Arbat <44476428+shivaniarbat@users.noreply.github.com>
2018-12-03 15:40:43 -0500

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -5,12 +5,14 @@ datagen = ImageDataGenerator(
          horizontal_flip=True,
          fill_mode='nearest')
 
+#list all the images present in the mentioned directory
 image_list = glob.glob('data/wooden-logs/train/labels/*')
 
-print(image_list)
+#print(image_list)
 
-print(len(image_list))
+#print(len(image_list))
 
+#for every image in the directory
 for index in range(0,len(image_list),1):
         img = load_img(image_list[index])
         print(img)
@@ -18,9 +20,10 @@ for index in range(0,len(image_list),1):
         x = x.reshape((1,) + x.shape)
 
         i = 0
-
+        # prefiex for the generated image will be 'logs' and format 'jpeg'
         for batch in datagen.flow(x,batch_size=1,
                         save_to_dir='data/wooden-logs/train/aug/labels', save_prefix='logs', save_format='jpeg'):
                 i += 1
                 if i > 2:
                         break
+# end of code 
